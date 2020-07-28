f9e7284b05010d7531186cc7f16fb59ead8b1a89
Oded Shopen <odedia@gmail.com>
2020-07-28 16:27:36 +0300

    removed rabbitmq

--- src/main/java/com/odedia/SpringBootTodoApplication.java
+++ src/main/java/com/odedia/SpringBootTodoApplication.java
@@ -1,19 +1,14 @@
 package com.odedia;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import io.micrometer.core.instrument.Gauge;
-import io.micrometer.core.instrument.MeterRegistry;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,14 +17,18 @@ import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sun.istack.NotNull;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @RestController
@@ -59,16 +58,16 @@ interface TodoRepository extends JpaRepository<Todo, Long> {
 
 @Slf4j
 @RepositoryEventHandler
-@EnableBinding(Source.class)
+//@EnableBinding(Source.class)
 class TaskEventHandler {
-	@Autowired Source source;
+//	@Autowired Source source;
 
 	@HandleBeforeSave
 	public void handleBeforeSave(Todo todo) {
 		log.info("Saving todo: {}", todo.getTitle());
 		if (todo.getCompleted()) {
-			log.info("Sending completed message for {}", todo.getTitle());
-			source.output().send(MessageBuilder.withPayload(todo.getTitle()).build());
+			log.info("This Todo is completed: {}", todo.getTitle());
+//			source.output().send(MessageBuilder.withPayload(todo.getTitle()).build());
 		}
 		log.info("Completed saving todo: {}", todo.getTitle());
 	}
