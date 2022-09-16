2a23ecd1351c904ffc065916c1a97f7223c08f6e
odedia <odedia@gmail.com>
2022-09-16 18:18:04 +0300

    tests

---
+++ src/test/java/com/odedia/SpringBootTodoTest.java
@@ -0,0 +1,14 @@
+package com.odedia;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootTodoTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
