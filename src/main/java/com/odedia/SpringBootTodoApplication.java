16c954e3895fefd60d6b1d6f08075c239e043442
Oded Shopen <odedia@gmail.com>
2022-11-28 14:54:26 +0200

    SBM: applied recipe 'boot-2.7-3.0-dependency-version-update'

--- src/main/java/com/odedia/SpringBootTodoApplication.java
+++ src/main/java/com/odedia/SpringBootTodoApplication.java
@@ -1,9 +1,9 @@
 package com.odedia;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
